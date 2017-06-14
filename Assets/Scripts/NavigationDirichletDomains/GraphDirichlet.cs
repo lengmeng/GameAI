ce6a08008e0d27f27369b85a96eb021de1551721
lengmeng <276237289@qq.com>
2017-06-15 00:56:58 +0800

    Signed-off-by: lengmeng <276237289@qq.com>

---
+++ Assets/Scripts/NavigationDirichletDomains/GraphDirichlet.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameAI.Navigation
+{
+    /// <summary>
+    /// 基于Dirichlet的Graph
+    /// </summary>
+    public class GraphDirichlet : Graph
+    {
+        Dictionary<int, List<int>> objToVertex;
+
+        /// <summary>
+        /// 添加Vertex 
+        /// </summary>
+        /// <param name="report"></param>
+        public void AddLocation(VertexReport report)
+        {
+            int objId = report.obj.GetInstanceID();
+            if (!objToVertex.ContainsKey(objId))
+            {
+                objToVertex.Add(objId, new List<int>());
+            }
+            objToVertex[objId].Add(report.vertex);
+        }
+
+        public void RemoveLocation(VertexReport report)
+        {
+            int objId = report.obj.GetInstanceID();
+            objToVertex[objId].Remove(report.vertex);
+        }
+    }
+}
---
+++ Assets/Scripts/NavigationDirichletDomains/VertexDirichlet.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameAI.Navigation
+{
+    /// <summary>
+    /// 基于Dirichlet的Vertex
+    /// </summary>
+    public class VertexDirichlet : Vertex
+    {
+        /// <summary>
+        /// 发生碰撞时，若是Agent或Player 则调用 AddLocaltion函数
+        /// </summary>
+        /// <param name="col"></param>
+        public void OnTriggerEnter(Collider col)
+        {
+            string objName = col.gameObject.name;
+
+            if(objName.Equals("Agent") || objName.Equals("Player"))
+            {
+                VertexReport report = new VertexReport(id, col.gameObject);
+                // 向物体和父物体发送消息（存在性能消耗）
+                SendMessageUpwards("AddLocaltion", report); 
+            }
+        }
+
+        /// <summary>
+        /// 碰撞结束时，若是Agent或Player 则调用 RemoveLocation函数
+        /// </summary>
+        /// <param name="col"></param>
+        public void OnTriggerExit(Collider col)
+        {
+            string objName = col.gameObject.name;
+            if (objName.Equals("Agent") || objName.Equals("Player"))
+            {
+                VertexReport report = new VertexReport(id, col.gameObject);
+                SendMessageUpwards("RemoveLocation", report);
+            }
+        }
+    }
+}
